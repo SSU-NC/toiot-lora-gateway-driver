2f9f49ed5fa9fac6af1155d78ab6107db238bc48
gjlee0802 <gjlee0802@naver.com>
2021-02-05 17:12:20 +0900

    add MQTT connection

--- app/GW/setup.py
+++ app/GW/setup.py
@@ -2,5 +2,5 @@ import argparse
 
 parser = argparse.ArgumentParser(description='toiot-lora-GW options')
 parser.add_argument('--b', required=True, help="broker-ip")
-parser.add_argument('--p', default = 1883, required=False, help="mqtt_port")
+parser.add_argument('--p', type=int, default = 1883, required=False, help="mqtt_port")
 args = parser.parse_args()
